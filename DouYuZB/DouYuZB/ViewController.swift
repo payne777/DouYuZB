2bae19321b1d9a3c2e17da99cacfe14645f36649
王鹏 <1597419892@qq.com>
2022-07-30 14:26:44 +0800

    Changes to be committed: 	new file:   DouYuZB/DouYuZB.xcodeproj/project.pbxproj 	new file:   DouYuZB/DouYuZB.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   DouYuZB/DouYuZB.xcodeproj/project.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist 	new file:   DouYuZB/DouYuZB/AppDelegate.swift 	new file:   DouYuZB/DouYuZB/Assets.xcassets/AccentColor.colorset/Contents.json 	new file:   DouYuZB/DouYuZB/Assets.xcassets/AppIcon.appiconset/Contents.json 	new file:   DouYuZB/DouYuZB/Assets.xcassets/Contents.json 	new file:   DouYuZB/DouYuZB/Base.lproj/LaunchScreen.storyboard 	new file:   DouYuZB/DouYuZB/Base.lproj/Main.storyboard 	new file:   DouYuZB/DouYuZB/Info.plist 	new file:   DouYuZB/DouYuZB/SceneDelegate.swift 	new file:   DouYuZB/DouYuZB/ViewController.swift 	new file:   DouYuZB/DouYuZBTests/DouYuZBTests.swift 	new file:   DouYuZB/DouYuZBUITests/DouYuZBUITests.swift 	new file:   DouYuZB/DouYuZBUITests/DouYuZBUITestsLaunchTests.swift
     Changes not staged for commit:
    	deleted:    DouYuZB/DouYuZB.xcodeproj/project.pbxproj
    	deleted:    DouYuZB/DouYuZB.xcodeproj/project.xcworkspace/contents.xcworkspacedata
    	deleted:    DouYuZB/DouYuZB.xcodeproj/project.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist
    	deleted:    DouYuZB/DouYuZB/AppDelegate.swift
    	deleted:    DouYuZB/DouYuZB/Assets.xcassets/AccentColor.colorset/Contents.json
    	deleted:    DouYuZB/DouYuZB/Assets.xcassets/AppIcon.appiconset/Contents.json
    	deleted:    DouYuZB/DouYuZB/Assets.xcassets/Contents.json
    	deleted:    DouYuZB/DouYuZB/Base.lproj/LaunchScreen.storyboard
    	deleted:    DouYuZB/DouYuZB/Base.lproj/Main.storyboard
    	deleted:    DouYuZB/DouYuZB/Info.plist
    	deleted:    DouYuZB/DouYuZB/SceneDelegate.swift
    	deleted:    DouYuZB/DouYuZB/ViewController.swift
    	deleted:    DouYuZB/DouYuZBTests/DouYuZBTests.swift
    	deleted:    DouYuZB/DouYuZBUITests/DouYuZBUITests.swift
    	deleted:    DouYuZB/DouYuZBUITests/DouYuZBUITestsLaunchTests.swift
    
     Untracked files:
    	DouYuZB.xcodeproj/
    	DouYuZB/AppDelegate.swift
    	DouYuZB/Assets.xcassets/
    	DouYuZB/Base.lproj/
    	DouYuZB/Info.plist
    	DouYuZB/SceneDelegate.swift
    	DouYuZB/ViewController.swift
    	DouYuZBTests/
    	DouYuZBUITests/

---
+++ DouYuZB/DouYuZB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DouYuZB
+//
+//  Created by 王鹏 on 2022/7/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
